33118347e90a181e58945197efc14c8b7bc2ae72
zongbinghuang <zongbinghuang@b.360.cn>
2017-12-06 20:51:30 +0800

    [change]delete some unusefull files.

--- djangos/blog/logic/list.py
+++
@@ -1,8 +0,0 @@
-from blog.models import List
-
-
-def getById(id):
-    try:
-        return List.objects.get(pk=id)
-    except List.DoesNotExist:
-        return False
--- djangos/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^index/$', views.index),
-    url(r'^news/(?P<id>[0-9]+)?$', views.news)
-]
